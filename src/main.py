3d3220f4d93a06d68670cfe42ef64c712a89c0ec
Elites100 <105951171+Elites100@users.noreply.github.com>
2024-01-27 15:57:33 -0600

    changes

--- src/main.py
+++ src/main.py
@@ -5,22 +5,40 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, validators, TextAreaField
 from wtforms.validators import DataRequired,  ValidationError
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-
-# making sure the selectfield is selected
 def validate_option(form, field):
     if field.data == 'choose':
-        raise ValidationError('Please select a option')
+        raise ValidationError('Please select an option.')
 
 class RecipeForm(FlaskForm):
-    recipe_name = StringField('Recipe Name', validators=[validators.DataRequired()])
-    ingredients = TextAreaField('Ingredients', validators=[validators.DataRequired()])
-    instructions = TextAreaField('Instructions', validators=[validators.DataRequired()])
+    recipe_name = StringField('Recipe Name', validators=[DataRequired()])
+    ingredients = TextAreaField('Ingredients', validators=[DataRequired()])
+    instructions = TextAreaField('Instructions', validators=[DataRequired()])
+    cuisine_type = SelectField('Cuisine Type', choices=[
+        ('choose', 'Choose'),
+        ('italian', 'Italian'),
+        ('mexican', 'Mexican'),
+        ('indian', 'Indian'),
+        ('chinese', 'Chinese'),
+        # Add more cuisine types as needed
+    ], default='choose', validators=[validate_option])
+    meal_type = SelectField('Meal Type', choices=[
+        ('choose', 'Choose'),
+        ('breakfast', 'Breakfast'),
+        ('lunch', 'Lunch'),
+        ('dinner', 'Dinner'),
+        ('snack', 'Snack'),
+        # Add more meal types as needed
+    ], default='choose', validators=[validate_option])
+    prep_time = StringField('Preparation Time (e.g., 30 minutes)', validators=[DataRequired()])
+    cook_time = StringField('Cooking Time (e.g., 1 hour)', validators=[DataRequired()])
+    servings = StringField('Number of Servings', validators=[DataRequired()])
     submit = SubmitField('Submit Recipe')
     
 
@@ -37,17 +55,19 @@ def internal_server_error(e):
 # Main webpage which is the form
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
-    form = RecipeForm()
-    if form.validate_on_submit():
-        form.vNumber.data= ''
-        form.name.data = ''
-        form.email.data = ''
-        form.SN.data = ''
-        form.CRN.data = ''
-        form.addtionalComment.data = ''
-        flash('The course override has been submitted', 'success')
-    return render_template('index.html', form=form, name=name)
+  name = None
+  form = RecipeForm()
+  if form.validate_on_submit():
+    # Here, you can process the form data, save it to a database, etc.
+    flash('Recipe submitted successfully!', 'success')
+    # Clear the form data after submission
+    form.recipe_name.data = ''
+    form.ingredients.data = ''
+    form.instructions.data = ''
+    form.prep_time.data = ''
+    form.cook_time.data = ''
+    form.servings.data = ''
+  return render_template('index.html', form=form, name=name)
 
 # Infomation page about the form
 @app.route('/about', methods=['GET'])
